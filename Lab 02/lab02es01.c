bddaed27648011c7ce1c0aac05245356b057d164
francescafornasier <fornasierarmando@mbpdifrancesca.lan>
2020-11-15 19:50:25 +0100

    updated

--- Lab 02/lab02es01.c
+++ Lab 02/lab02es01.c
@@ -11,49 +11,47 @@ void show_directory(const char path[], char fill[]);
 
 
 int main(int argc, const char * argv[]) {
-    if (argc!=2) {
-        fprintf(stderr, "Error: expected programExe directoryPath\n");
-        exit(1);
-    }
-    show_directory(argv[1], "");
-    return 0;
+     if (argc!=2) {
+          fprintf(stderr, "Error: expected programExe directoryPath\n");
+          exit(1);
+     }
+     show_directory(argv[1], "");
+     return 0;
 }
 
 
 void show_directory(const char path[], char fill[]) {
-    struct stat sb;
-    struct dirent *dirp;
-    DIR *dp;
-    char new_path[MAXLENGHT];
-    char new_fill[MAXLENGHT];
+     struct stat sb;
+     struct dirent *dirp;
+     DIR *dp;
+     char new_path[MAXLENGHT];
+     char new_fill[MAXLENGHT];
 
-    if (S_ISDIR(sb.st_mode)) {
-        if ((dp = opendir(path)) == NULL){
-            fprintf(stderr, "Error opening directory %s\n", path);
-            exit(-1);
-        }
+     if ((dp = opendir(path)) == NULL){
+          fprintf(stderr, "Error opening directory %s\n", path);
+          exit(-1);
+     }
 
-        while ((dirp = readdir(dp)) != NULL){
-            if (stat(path, &sb) < 0) {
-                fprintf(stderr, "Error\n");
-            }
-            if (S_ISREG(sb.st_mode)){
-                printf("File: %s\n", dirp->d_name);
-            }
-            else if (S_ISDIR(sb.st_mode)) {
-                if(!strcmp("/.", dirp->d_name) || !strcmp("/..", dirp->d_name)){
+     while ((dirp = readdir(dp)) != NULL){
+          if (stat(path, &sb) < 0) {
+               fprintf(stderr, "Error\n");
+          }
+          if (S_ISREG(sb.st_mode)){
+               printf("File: %s\n", dirp->d_name);
+          }
+          else if (S_ISDIR(sb.st_mode)) {
+               if(!strcmp("/.", dirp->d_name) || !strcmp("/..", dirp->d_name)){
                     printf("Directory: %s\n", dirp->d_name);
-                }
-                else {
+               }
+               else {
                     printf("Directory: %s\n", dirp->d_name);
                     sprintf(new_path, "%s%s", path, dirp->d_name);
                     sprintf(new_fill, "%s%s", fill, "   ");
                     show_directory(new_path, new_fill);
-                }
-            }
-        }
-        closedir(dp);
-    }
+               }
+          }
+     }
+     closedir(dp);
 
-    return;
+     return;
 }
